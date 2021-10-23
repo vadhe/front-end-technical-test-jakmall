3bb2569052c6ddef81e99c3b3a5f353340fb231d
vadhe <aldyvadhe@gmail.com>
2021-10-23 14:08:00 +0800

    create layoutstyled and containerstyled

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,28 +1,56 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import Delivery from "../components/delivery/Delivery";
-import Finish from "../components/finish/Finish";
-import Payment from "../components/payment/Payment";
+import Delivery from "../pages/Delivery";
+import Finish from "../pages/Finish";
+import Payment from "../pages/Payment";
 import React from "react";
+import Step from "../components/Step";
+import styled from "styled-components";
 
 const Layout = () => {
   return (
     <Router>
-      <div>
+      <LayoutStyled>
+        <div className="wrap-step">
+          <Step />
+          <Step />
+          <Step />
+        </div>
         <Switch>
-          <Route exact path="/">
-            <Delivery />
-          </Route>
-          <Route path="/payment">
-            <Payment />
-          </Route>
-          <Route path="/finish">
-            <Finish />
-          </Route>
+          <ContainerStyled>
+            <Route exact path="/">
+              <Delivery />
+            </Route>
+            <Route path="/payment">
+              <Payment />
+            </Route>
+            <Route path="/finish">
+              <Finish />
+            </Route>
+          </ContainerStyled>
         </Switch>
-      </div>
+      </LayoutStyled>
     </Router>
   );
 };
 
 export default Layout;
+const LayoutStyled = styled.div`
+  background-color: ${({ theme }) => theme.colors.leadGlass};
+  padding-top: 2rem;
+  .wrap-step {
+    display: none;
+  }
+  @media (min-width: 1024px) {
+    .wrap-step {
+      padding: 2rem;
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
+const ContainerStyled = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  height: 550px;
+  padding: 1rem;
+`;
